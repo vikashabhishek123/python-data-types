03f74a8272cae166539874e006740228e4261bae
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-17 22:26:51 +0530

    on the way

--- Dictionary.py
+++ Dictionary.py
@@ -15,3 +15,9 @@ if "Black" in chai_types:
     else:
     print("Black tea is not available")
 
+#  chai_types["White"]="Smooth"       ("this will add a new key-value pair to the dictionary")
+
+chai_types.pop("Oolong")  # this will remove the key-value pair with key "Oolong"
+
+chai_types.clear()  # this will remove all key-value pairs from the dictionary
+chai_types.update({"Herbal": "Soothing", "Chai Latte": "Creamy"})  # this will add new key-value pairs to the dictionary
