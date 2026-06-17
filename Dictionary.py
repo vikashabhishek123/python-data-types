89ef24878903d244770be033b8a085bd5d16d207
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-17 22:19:41 +0530

    phir kar update

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,17 @@
-# today we will cover dictionary in python
+today we will cover dictionary in python
 
 chai_types={"Black": "Strong", "Green": "Mild", "Oolong": "Rich"}
 print(chai_types)
+
+chai_types["Green"]="Delicate"
+
+for chai in chai_types:
+    print(chai, chai_types[chai])
+
+
+if "Black" in chai_types:
+    print("Black tea is available")
+
+    else:
+    print("Black tea is not available")
+
