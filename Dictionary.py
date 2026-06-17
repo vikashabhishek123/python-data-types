f13f698c84b1911ae6653fbcc0dcc2b270c5073c
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-17 22:52:03 +0530

    keep updating

--- Dictionary.py
+++ Dictionary.py
@@ -21,3 +21,13 @@ chai_types.pop("Oolong")  # this will remove the key-value pair with key "Oolong
 
 chai_types.clear()  # this will remove all key-value pairs from the dictionary
 chai_types.update({"Herbal": "Soothing", "Chai Latte": "Creamy"})  # this will add new key-value pairs to the dictionary
+del chai_types["Green"]  # this will remove the key-value pair with key "Green"
+
+tea_shop={
+    "chai":{"Masala":"Spicy", "Ginger":"Zesty"},
+    "coffee":{"Espresso":"Strong", "Latte":"Creamy"},
+    "pastries":{"Croissant":"Flaky", "Muffin":"Soft"}
+    }
+
+    squared_numbers={x:x**2 for x in range(6)}  # this will create a dictionary with numbers from 1 to 5 as keys and their squares as values
+print(squared_numbers)
