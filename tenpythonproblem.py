93569626af5d0984d7fae4e6fae0f1abded32c2c
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-18 13:13:44 +0530

    end

--- tenpythonproblem.py
+++ tenpythonproblem.py
@@ -104,3 +104,66 @@ elif distance >15:
 else:
     print("Invalid distance entered.")
 
+
+#Question7
+#Customize a coffee order based on the customer's preferences.
+
+order_size = input("Enter the size of your coffee (small, medium, large): ")
+milk_preference = input("Do you want milk in your coffee? (yes/no): ")
+
+if order_size == "small":
+    if milk_preference == "yes":
+        print("You ordered a small coffee with milk.")
+    else:
+        print("You ordered a small black coffee.")
+
+elif order_size == "medium":
+    if milk_preference == "yes":
+        print("You ordered a medium coffee with milk.")
+    else:
+        print("You ordered a medium black coffee.")
+
+elif order_size == "large":
+    if milk_preference == "yes":
+        print("You ordered a large coffee with milk.")
+    else:
+        print("You ordered a large black coffee.")
+
+else:
+    print("Invalid coffee size entered.")
+
+    #Question8
+    #Check password strength like weak, medium, strong based on certain criteria.
+
+password = input("Enter your password: ")
+if len(password) < 6:
+    print("weak passwoed")
+
+elif len(password) < 12:
+    print("medium password")
+
+elif len(password) >= 12:
+    print("strong password")
+
+else:
+    print("invalid password")
+
+#Question9
+#Determine if a year is leap year or not.
+
+year = int(input("Enter a year: "))
+ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+
+else:
+    print(year, "is not a leap year.")
+
+#Question10
+#Puppy food ,e.g. Dog <2 years - puppy food, Dog >=2 years - adult dog food.
+dog_age = float(input("Enter the age of the dog in years: "))
+
+if dog_age < 2:
+    print("Feed the dog puppy food.")
+
+else:
+    print("Feed the dog adult dog food.")
