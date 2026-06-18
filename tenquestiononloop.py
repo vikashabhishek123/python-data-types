796c399267dd1d700abb9709f0e6762eeac9d7ef
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-18 23:47:57 +0530

    new

--- tenquestiononloop.py
+++ tenquestiononloop.py
@@ -9,3 +9,29 @@ for num in numbers:
 
 print("The count of positive numbers in the list is:", positive_numbers_count)
 
+
+#Question 2
+#sum of even numbers 
+
+n=10
+sum_even=0
+
+for i in range(1, n+1):
+    if i % 2 == 0:
+        sum_even += 1
+
+print("The sum of even numbers from 1 to", n, "is:", sum_even)
+
+
+#Question 3q    
+#print the multiplication table of a given number
+number =3
+
+for i in range(1, 11):
+    if i==5:
+        continue
+    result = number * i
+    print(number, "x", i, "=", result)
+
+#question 4
+#reverse a string using loop 
