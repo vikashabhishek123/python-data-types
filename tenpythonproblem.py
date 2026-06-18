8b252e40a1e16ec94307d144c1db08b7da73a270
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-18 12:23:02 +0530

    new

--- tenpythonproblem.py
+++ tenpythonproblem.py
@@ -10,3 +10,37 @@ elif age<20:
 elif age<60:
     print("adult")
 
+else:
+    print("senior citizen")
+
+
+
+#question2
+age=22
+day = "Wednesday"
+
+price =12 if age >= 18 else 8
+
+if day == "Wednesday":
+    price = price - 2
+
+print("ticket price is $" , price)
+
+
+#question3
+marks=89
+
+if marks >=90:
+    print("A grade")
+
+elif marks >=80:
+    print("B grade")
+
+elif marks >=70:
+    print("C grade")
+
+elif marks >=60:   
+    print("D grade")
+
+else:
+    print("failed")
