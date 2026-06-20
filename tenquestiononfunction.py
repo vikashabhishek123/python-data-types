71d44c5f03f23c932b41cbe4fded70b5005eb690
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-20 16:16:36 +0530

    new

---
+++ tenquestiononfunction.py
@@ -0,0 +1,15 @@
+#q1 write a function to calculate and return square of a number
+def square(number):
+    return number ** 2
+
+    result = square(5)
+    print(result)
+
+#q2 write a function which takes two no. as a parameter and return the sum of two numbers
+
+def two_number(number1,number2):
+    return number1+number2
+
+result = two_number(5,10)
+print("the sum of two numbers is:", result)
+
