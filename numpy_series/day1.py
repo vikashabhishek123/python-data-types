047e1b74559886609126b9bb823bc0365574a406
vikashabhishek123 <vikashabhishek123@gmail.com>
2026-06-25 09:40:00 +0530

    new

---
+++ numpy_series/day1.py
@@ -0,0 +1,16 @@
+"""
+This is a demo file for numpy series in python.
+here we learn about indexing and slicing of numpy series. 
+
+array[index] #1d array
+array[rows , columns] #2d array
+
+"""
+#here we learn about indexing of numpy series.
+import numpy as np
+arr =np.array([44,67,34,24,67])
+print(arr[-1]) #last element
+print(arr[0]) #first element
+
+
+#here we learn about slicing of numpy series.
